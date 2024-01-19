30c04b1da1f75965d7bc8e2e14eab1117d2348b9
sachin <mrsm492002@gmail.com>
2024-01-19 20:49:49 +0530

    added support page

--- app/StoreProvider.tsx
+++ app/StoreProvider.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import { Provider } from "react-redux";
 import { store } from "../lib/store/store";
 
-export default function Providers({ children }) {
+export default function Providers({children,
+}: {
+  children: React.ReactNode
+}) {
   return <Provider store={store}>{children}</Provider>;
 }
